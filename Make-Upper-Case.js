54c44eecf6271557095dae596bed106abd59d900
d0ntg3tb0ld <d0ntg3tb0ld@proton.me>
2022-10-20 20:20:08 -0400

    added new code challenge

---
+++ Make-Upper-Case.js
@@ -0,0 +1,8 @@
+//Write a function which converts the input string to uppercase.
+
+//My Solution
+
+function makeUpperCase(str) {
+    let toUpperCase = str.toUpperCase()
+    return toUpperCase
+  }
