a9b17a25fe055c76e1cb6ee137b27c4de62d10d0
d0ntg3tb0ld <d0ntg3tb0ld@proton.me>
2022-10-20 20:05:26 -0400

    added new codewars challenge

---
+++ Multiply.js
@@ -0,0 +1,12 @@
+// This code does not execute properly. Try to figure out why.
+//  function multiply(a, b){
+//    a * b
+//  }
+
+//My Solution
+
+function multiply(a, b){
+    let sum = a * b
+    return sum
+  }
+  multiply(5, 10)
